b98d9f6fb5076251db18d8e33a72f88df627f8a1
Zeinab1618 <zeinab.elsayed325@gmail.com>
2025-07-27 07:50:03 +0300

    Update dorm_streamlit.py

--- dorm_streamlit.py
+++ dorm_streamlit.py
@@ -8,7 +8,9 @@ conn = mysql.connector.connect(
     host=st.secrets["mysql"]["host"],
     user=st.secrets["mysql"]["user"],
     password=st.secrets["mysql"]["password"],
-    database=st.secrets["mysql"]["database"]
+    database=st.secrets["mysql"]["database"],
+    port=st.secrets["mysql"]["port"],
+    ssl_ca=st.secrets["mysql"]["ssl_ca"]
 )
 cursor = conn.cursor(dictionary=True)
 
